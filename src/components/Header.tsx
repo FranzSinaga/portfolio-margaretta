5d8fa16c927a3be21ff963aa3397394be8046626
Franz Sinaga <sinagafranz12@gmail.com>
2024-04-14 17:59:33 +0700

    fix: active menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,20 @@
-import { graphql, useStaticQuery } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
-import React, { PropsWithChildren } from "react";
-import { useState } from "react";
-import { twMerge } from "tailwind-merge";
+import React from "react";
+import { Link } from "gatsby";
 
 type NavigationListType = {
   name: string;
   href: string;
-  id: string;
 }[];
 
 const navigation: NavigationListType = [
-  { name: "Home", href: "/", id: "home" },
-  { name: "Education", href: "/education", id: "education" },
-  { name: "Work Experience", href: "/work-experience", id: "work-experience" },
-  { name: "Projects", href: "/projects", id: "projects" },
-  { name: "Social Media", href: "/socials", id: "socials" },
+  { name: "Home", href: "/" },
+  { name: "Education", href: "/education" },
+  { name: "Work Experience", href: "/work-experience" },
+  { name: "Projects", href: "/projects" },
+  { name: "Social Media", href: "/socials" },
 ];
 
 export const Header = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  const url = typeof window !== "undefined" ? window.location.pathname : "/";
-
-  const processUrl = (param: string) => {
-    return url === "/" ? "home" : url.split("/")[1] === param && param;
-  };
-  console.log(url);
   return (
     <header className="py-2">
       <nav
@@ -40,18 +28,15 @@ export const Header = () => {
             className="w-10 mr-10"
           /> */}
           {navigation.map((item, key) => (
-            <a
+            <Link
               key={key}
               id={item.name}
-              href={item.href}
-              className={twMerge(
-                "text-lg font-semibold leading-6 text-gray-800 hover:text-purple-700 hover:bg-gray-100 p-2 px-3 hover:rounded-full",
-                processUrl(item.id) === item.id &&
-                  "text-purple-700 border-b-2 border-purple-700 border-spacing-3 hover:rounded-none hover:bg-white"
-              )}
+              to={item.href}
+              className="text-lg font-semibold leading-6 text-gray-800 hover:text-purple-700 hover:bg-gray-100 p-2 px-3 hover:rounded-full"
+              activeClassName="text-purple-700 border-b-2 border-purple-700 border-spacing-3 hover:rounded-none hover:bg-white"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
       </nav>
