3115e8a473ca23a67016782f840faf9d0dad4321
Franz Sinaga <sinagafranz12@gmail.com>
2024-04-24 13:42:45 +0700

    feat: add icon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,8 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `Margaretta Portfolio's`,
     siteUrl: `https://www.yourdomain.tld`,
+    description:
+      "Experience the quality and precision of a seasoned Technical Writer and Quality Assurance specialist. Explore the portfolio where meticulous writing meets rigorous quality control, showcasing a commitment to excellence and attention to detail in every project.",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -14,6 +16,7 @@ const config: GatsbyConfig = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-offline",
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -21,6 +24,18 @@ const config: GatsbyConfig = {
         path: "./src/",
       },
       __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Margaretta Portfolio's`,
+        short_name: `Margaretta`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#483285`,
+        display: `standalone`,
+        icon: `src/images/logo_etha.png`,
+      },
     },
   ],
 };
